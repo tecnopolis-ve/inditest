178e9a16acad35ce58ebe444c58b844ab18e1eb2
Manuel <manuel_se@live.com>
2022-05-26 16:39:22 +0200

    main logic, wip

--- src/task.js
+++ src/task.js
@@ -20,6 +20,44 @@ const checkFile = (req, res, next) => {
     next();
 };
 
+const getFilenameAndExtension = (pathfilename) => {
+    return [path.parse(pathfilename).name, path.parse(pathfilename).ext];
+};
+
+const resize = async (image, width) => {
+    const fileProperties = getFilenameAndExtension(image);
+    const outputPath = `./output/${fileProperties[0]}/${width}`;
+
+    try {
+        if (!fs.existsSync(outputPath)) {
+            fs.mkdirSync(outputPath, { recursive: true });
+        }
+
+        const rawImage = await sharp(image)
+            .resize({
+                width,
+            })
+            .toBuffer();
+
+        const md5 = crypto.createHash("md5").update(rawImage).digest("hex");
+        const toFile = `${outputPath}/${md5}${fileProperties[1]}`;
+
+        const saveFile = fs.createWriteStream(toFile);
+        saveFile.write(rawImage);
+        saveFile.end();
+        saveFile.on("finish", async function () {
+            console.log("ok");
+        });
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+const getImageInfo = async (image) => {
+    const readImg = sharp(image);
+    return await readImg.metadata();
+};
+
 const getTasks = async (req, res) => {
     const tasks = await Task.findAll();
 
@@ -27,7 +65,19 @@ const getTasks = async (req, res) => {
 };
 
 const getTask = async (req, res) => {
-    res.send("getTask");
+    const id = req.params.id;
+
+    const task = await Task.findOne({
+        where: {
+            id,
+        },
+    });
+
+    if (task) {
+        return res.send(task);
+    }
+
+    res.status(404).send("Imagen no encontrada");
 };
 
 const postProcessImages = async (req, res) => {
@@ -35,15 +85,56 @@ const postProcessImages = async (req, res) => {
         where: { processed: false },
     });
 
-    for (const i in tasks){
-        console.log(tasks[i]);
+    const resolutions = [800, 1024];
+    let newTasks = [];
+
+    for (const i in tasks) {
+        for (const j in resolutions) {
+            newTasks.push(resize(tasks[i].path, resolutions[j]));
+        }
     }
+
+    Promise.all([newTasks]).then((values) => {
+        console.log("All done");
+    });
 
     res.send("postProcessImages");
 };
 
 const postTask = async (req, res) => {
-    res.send("postTask");
+    let uploadedFile;
+    let uploadPath = "./input/";
+
+    try {
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath);
+        }
+    } catch (error) {
+        console.log("error", error);
+    }
+
+    uploadedFile = req.files[0];
+    uploadPath = uploadPath + uploadedFile.originalname;
+
+    const saveFile = fs.createWriteStream(uploadPath);
+
+    saveFile.write(uploadedFile.buffer);
+    saveFile.end();
+    saveFile.on("finish", async function () {
+        const imageInfo = await getImageInfo(uploadedFile.buffer);
+        const md5 = crypto
+            .createHash("md5")
+            .update(uploadedFile.buffer)
+            .digest("hex");
+
+        await Task.create({ path: uploadPath, status: "RAW", process: false });
+        await Image.create({
+            path: uploadPath,
+            resolution: `${imageInfo.width}x${imageInfo.height}`,
+            md5,
+        });
+        res.send(`Imagen cargada correctamente`);
+    });
 };
 
 router.get("/", getTasks);
