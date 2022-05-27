16c4f01a086fbdb45a639e95d3770d5e2203723b
Manuel <manuel_se@live.com>
2022-05-27 12:49:24 +0200

    docker

--- app/config/db.js
+++ app/config/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const env = process.env.NODE_ENV || "cloud";
+const env = process.env.NODE_ENV || "development";
 const config = require("./config.json")[env];
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/index');
-const port = 3000;
-
-const server = app.mainApp.listen(port, () => {
-    console.log(`Escuchando en http://localhost:${port}`);
-});
-
-module.exports = server
